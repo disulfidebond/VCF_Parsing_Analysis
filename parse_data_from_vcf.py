51f3c4fd273726701b539e704c39bec2078b329a
John Caskey <disulfidebond@gmail.com>
2019-07-31 11:26:46 -0500

    Update parse_data_from_vcf.py

--- parse_data_from_vcf.py
+++ parse_data_from_vcf.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-vcfDF = pd.read_csv('Cyno.box13.snps.hg38.geneListFiltered.vcf.tsv', sep='\t')
+vcfDF = pd.read_csv('some_vcf_file.vcf.tsv', sep='\t')
 vcfDF.head()
 
 # create list with column IDs
@@ -40,10 +40,10 @@ parsed_samplesDF_vcf['40156'].value_counts()
 Name: 40156, dtype: int64
 
 # check again with Bash from Jupyter Notebook, remove the '!' to run in a Bash session
-!gzcat Cyno.box13.snps.hg38.geneListFiltered.vcf.gz | tail -n 895 | cut -d$'\t' -f10 | grep -c '^0/0'
-!gzcat Cyno.box13.snps.hg38.geneListFiltered.vcf.gz | tail -n 895 | cut -d$'\t' -f10 | grep -c '^0/1'
-!gzcat Cyno.box13.snps.hg38.geneListFiltered.vcf.gz | tail -n 895 | cut -d$'\t' -f10 | grep -c '^1/1'
-!gzcat Cyno.box13.snps.hg38.geneListFiltered.vcf.gz | tail -n 895 | cut -d$'\t' -f10 | grep -c '^1/0'
+!gzcat some_vcf_file.vcf.tsv.gz | tail -n 895 | cut -d$'\t' -f10 | grep -c '^0/0'
+!gzcat some_vcf_file.vcf.tsv.gz | tail -n 895 | cut -d$'\t' -f10 | grep -c '^0/1'
+!gzcat some_vcf_file.vcf.tsv.gz | tail -n 895 | cut -d$'\t' -f10 | grep -c '^1/1'
+!gzcat some_vcf_file.vcf.tsv.gz | tail -n 895 | cut -d$'\t' -f10 | grep -c '^1/0'
 
 # outputs
 505
